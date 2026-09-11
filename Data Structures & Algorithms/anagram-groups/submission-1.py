ebe266822cc648249ff7cb4028d426fed9bd8a82
Shaurya Maan Gusain <156488564+smgusain24@users.noreply.github.com>
2026-09-11 19:43:59 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hmap = defaultdict(list)
+        result = []
+        for s in strs:
+            count = [0]*26
+            for c in s:
+                count[ord(c)-ord("a")] += 1 
+            hmap[tuple(count)].append(s)
+        return list(hmap.values())
